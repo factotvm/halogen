d0c3c50646061e32ed603775b026a8af4329626f
Daniel Thompson <daniel@ionize.net>
2022-02-05 14:38:36 -0500

    Add functionality around source and destination
    For now, this just locks the source and destination folders to values
    ("content" and "public", respectively). But, it also adds tests around
    the behavior: no source should cause a panic, and no destination will
    create the destination directory.
    
    To be honest, the panic isn't tested. I'm not exactly sure how to
    achieve that. But I did pull the work out into a function that can be
    tested to ensure that a destination directory is created, etc.
    
    Additionally, I added two tasks (with explanations) to the README, with
    one of them being completed (which you're reading about).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::fmt::Error;
+use std::fs;
+use std::path::Path;
+
+const SRC_DIR: &str = "content";
+const DEST_DIR: &str = "public";
+
 fn main() {
-    println!("Hello, world!");
+    if check_dir(SRC_DIR, false).is_ok() {
+        println!("Using source directory: {}.", SRC_DIR)
+    } else {
+        panic!("The source directory ({}) is not present.", SRC_DIR)
+    }
+
+    if check_dir(DEST_DIR, true).is_ok() {
+       println!("Using destination directory: {}.", DEST_DIR);
+    } else {
+        panic!("The destination directory could not be created ({}).", DEST_DIR)
+    }
+}
+
+fn check_dir(path: &str, create_if_needed: bool) -> Result<(), Error> {
+    if !Path::new(path).is_dir() {
+        if create_if_needed {
+            println!("Creating directory: {}", path);
+            fs::create_dir_all(path).unwrap()
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::{SystemTime, UNIX_EPOCH};
+    use super::*;
+
+    #[test]
+    fn check_dir_exist() {
+        let src = create_path();
+        fs::create_dir_all(src.clone()).expect("temporary file should be created");
+        assert_eq!(check_dir(&src, false), Ok(()));
+    }
+
+    #[test]
+    fn check_dir_will_not_create_dir() {
+        let src = create_path();
+        assert_eq!(check_dir(&src, false), Ok(()));
+        assert!(!Path::new(&src).is_dir());
+    }
+
+    #[test]
+    fn check_dir_will_create_dir() {
+        let src = create_path();
+        assert_eq!(check_dir(&src, true), Ok(()));
+        assert!(Path::new(&src).is_dir());
+    }
+
+    fn create_path() -> String {
+        match SystemTime::now().duration_since(UNIX_EPOCH) {
+            Ok(n) => format!("/tmp/src-{}", n.as_nanos()),
+            _ => {panic!("unable to create timestamp for test path")}
+        }
+    }
 }
